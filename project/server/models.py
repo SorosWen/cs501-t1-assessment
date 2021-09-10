0b74ae0550b6ad94301a416345a30cb50de1d501
Soros Wen <wenmingjun0725@gmail.com>
2021-09-10 13:43:30 -0400

    Update models.py

--- project/server/models.py
+++ project/server/models.py
@@ -1,7 +1,7 @@
 #import jwt
 import datetime
 
-from project.server import app, db, bcrypt
+from project.server import db, bcrypt
 
 class User(db.Model):
     """ User Model for storing user related details """
